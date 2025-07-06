721f2a55efc80700d69f42b10bf1bba5f18656e2
Ferrid <farid.calalov25@gmail.com>
2025-07-06 13:41:18 +0400

    Dongu veririkki mushteri 16 reqemden az ve ya cox yazsa yeniden imput istesin

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Kartin 16 reqemini daxil et:");
         long cardNumber = new Scanner(System.in).nextLong();
+        for (;9999999999999999l<cardNumber || cardNumber<1000000000000000l; ){
+            System.out.println("Bir daha yoxlayin");
+            cardNumber = new Scanner(System.in).nextLong();
+        }
         
 
     }
